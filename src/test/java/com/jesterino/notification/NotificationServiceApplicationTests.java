7cb3553cd4bbe61f651d7f47a937bf3ef9c7c33a
JesterinoInBox <toliksankin@gmail.com>
2026-07-19 16:57:04 +0300

    init

---
+++ src/test/java/com/jesterino/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesterino.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
