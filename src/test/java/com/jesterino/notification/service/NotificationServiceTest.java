056d55a3185c9bed7cb97f1381c9a7294c33ee90
JesterinoInBox <toliksankin@gmail.com>
2026-07-27 22:26:36 +0300

    Добавлен integration test

--- src/test/java/com/jesterino/notification/service/NotificationServiceTest.java
+++ src/test/java/com/jesterino/notification/service/NotificationServiceTest.java
@@ -1,54 +1,53 @@
-//package com.jesterino.notification.service;
-//
-//import com.jesterino.notification.event.TaskCreatedEvent;
-//import com.jesterino.notification.strategy.NotificationType;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.kafka.test.context.EmbeddedKafka;
-//import org.springframework.test.context.bean.override.mockito.MockitoBean;
-//
-//import java.time.Duration;
-//
-//import static org.awaitility.Awaitility.await;
-//import static org.mockito.Mockito.verify;
-//
-//@SpringBootTest
-//@EmbeddedKafka(
-//        partitions = 1,
-//        topics = "task-events"
-//)
-//class NotificationServiceTest {
-//
-//    @Autowired
-//    KafkaTemplate<String, TaskCreatedEvent> kafkaTemplate;
-//
-//    @MockitoBean
-//    NotificationService notificationService;
-//
-//    @Test
-//    void shouldConsumeKafkaMessage() {
-//
-//        TaskCreatedEvent event =
-//                new TaskCreatedEvent(
-//                        1L,
-//                        "Task",
-//                        "John",
-//                        "EMAIL",
-//                        "TODO",
-//                        NotificationType.EMAIL
-//                );
-//
-//        kafkaTemplate.send("task-events", event);
-//
-//        await()
-//                .atMost(Duration.ofSeconds(5))
-//                .untilAsserted(() ->
-//                        verify(notificationService)
-//                                .send(
-//                                        NotificationType.EMAIL,
-//                                        "Task created"
-//                                ));
-//    }
-//}
+package com.jesterino.notification.service;
+
+import com.jesterino.notification.event.TaskCreatedEvent;
+import com.jesterino.notification.strategy.NotificationType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.time.Duration;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
+})
+@EmbeddedKafka(partitions = 1, topics = "task-events")
+class NotificationServiceTest {
+
+    private static final String TOPIC = "task-events";
+
+    @Autowired
+    private KafkaTemplate<String, TaskCreatedEvent> kafkaTemplate;
+
+    @MockitoBean
+    private NotificationService notificationService;
+
+    @Test
+    void shouldInvokeNotificationServiceWhenTaskCreatedEventReceived() throws Exception {
+
+        TaskCreatedEvent event = new TaskCreatedEvent(
+                1L,
+                "Task",
+                "John",
+                "EMAIL",
+                "TODO",
+                NotificationType.EMAIL
+        );
+
+        kafkaTemplate.send(TOPIC, event).get();
+
+        await()
+                .atMost(Duration.ofSeconds(5))
+                .untilAsserted(() ->
+                        verify(notificationService)
+                                .send(NotificationType.EMAIL, "Task created"));
+    }
+}
